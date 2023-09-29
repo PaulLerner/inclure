aa7e74a097c53b5af8b2b4c0284054fa562e835a
Paul Lerner <lerner@tuta.io>
2023-09-29 16:58:25 +0200

    BREAKING: switch separator to dot (not median) to match Barthez tokenizer

--- inclure/common.py
+++ inclure/common.py
@@ -8,16 +8,17 @@ register_type(Path)
 
 
 extra_spaces = re.compile(r'\s+')
-SEP = "·"
+SEP = r"\."
 # same as "\w" but does not match digits
 LETTER = r"[^\d\W]"
-sep_sub = rf"\1{SEP}\2"
-extra_seps = re.compile(rf'({LETTER})[\.•]({LETTER})')
+sep_sub = rf"\1.\2"
+extra_seps = re.compile(rf'({LETTER})[·•]({LETTER})')
+
 
 def preproc(text):
     # common preproc: strip extra spaces
     text = extra_spaces.sub(' ', text.strip())
-    # keep a single separator : "·"
+    # keep a single separator : "."
     text = extra_seps.sub(sep_sub, text)
     # hack : run twice in case of things like "tou.s.tes" where "u.s" matching will prevent "s.t" matching
     text = extra_seps.sub(sep_sub, text)
