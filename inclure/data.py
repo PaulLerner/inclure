149f70b75cfcd30347b64794bc26c328fd0eb139
Paul Lerner <lerner@tuta.io>
2023-10-02 12:41:56 +0200

    fix: data: pandas is boring

--- inclure/data.py
+++ inclure/data.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import pandas as pd
 from datasets import Dataset
 
 from .common import CLI, Path
@@ -9,9 +8,14 @@ from .common import CLI, Path
 def main(data_root_path: Path):
     data = []
     for path in data_root_path.glob('*.tsv'):
-        training_pairs = pd.read_csv(path, sep='\t', header=None)
-        for _, pair in training_pairs.iterrows():
-            data.append({'fri': pair[0], 'fr': pair[1]})
+        with open(path, 'rt') as file:
+            for line in file.readlines():
+                if not line:
+                    continue
+                pair = line.split("\t")
+                if len(pair) != 2:
+                    breakpoint()
+                data.append({'fri': pair[0], 'fr': pair[1]})
     dataset = Dataset.from_dict({'translation': data})
     print(dataset)
     dataset = dataset.train_test_split(test_size=0.2, seed=0)
